2014500f1b3696f2546667751ed7fd6a9578fe27
derkian <rderkian@gmail.com>
2019-09-10 13:24:41 -0300

    Rules Engine

---
+++ RuleSharp/RuleSharp.RuleEngine/RuleRunner.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using Mono.CSharp;
+
+//Install-Package Mono.CSharp
+namespace RuleSharp.RuleEngine
+{
+    public class RuleRunner
+    {
+        private readonly Evaluator _evaluator;
+
+        private readonly List<Assembly> _assemblies = new List<Assembly>
+        {
+            Assembly.GetCallingAssembly(),
+            Assembly.GetExecutingAssembly()
+        };
+
+        private readonly List<string> _namespaces = new List<string>
+        {
+            "System",
+            "System.Linq",
+            "System.Collections.Generic",
+            "System.Threading.Tasks",
+            "System.Text"
+        };
+
+        public RuleRunner(IEnumerable<Assembly> assemblies = null, IEnumerable<string> namespaces = null)
+        {
+            if (assemblies != null)
+                _assemblies.AddRange(assemblies);
+
+            if (namespaces != null)
+                _namespaces.AddRange(namespaces);
+
+            _evaluator = new Evaluator(new CompilerContext(new CompilerSettings(), new ConsoleReportPrinter()));
+
+            _assemblies.ForEach(a => _evaluator.ReferenceAssembly(a));
+            _namespaces.ForEach(ns => _evaluator.Run("using " + ns + ";"));
+
+
+        }
+
+        /// <summary>
+        /// Evaluates a single C# statement to true or false
+        /// </summary>
+        /// <typeparam name="TInput">Any custom class</typeparam>
+        /// <param name="statement">A C# statement</param>
+        /// <param name="input">Custom object of type TInput</param>
+        /// <returns>True of false</returns>
+        public bool IsStatementTrue<TInput>(string statement, TInput input) where TInput : class
+        {
+            return Evaluate(statement.TrimEnd(';'), input, false);
+        }
+
+        /// <summary>
+        /// Executes a block of C# statement that should return either true or false
+        /// </summary>
+        /// <typeparam name="TInput">Any custom class</typeparam>
+        /// <param name="block">One or more C# statements including a return</param>
+        /// <param name="input">Custom object of type TInput</param>
+        /// <returns>True of false</returns>
+        public bool IsBlockTrue<TInput>(string block, TInput input) where TInput : class
+        {
+            return Evaluate(block, input, true);
+        }
+
+        
+        private bool Evaluate<TInput>(string input, TInput inputObject, bool isBlock) where TInput : class 
+        {
+            if (inputObject is Array || inputObject is IEnumerable)
+            {
+                throw new ArgumentException("Input must not be an array or list");
+            }
+  
+            var inputType = typeof(TInput).FullName;
+            string expression = isBlock ? "new Func<{0}, bool>((input) => {{ {1} }});" : "new Func<{0}, bool>(input => {1});";
+            string formattedExpression = string.Format(expression, inputType, input);
+            var func = (Func<TInput, bool>)_evaluator.Evaluate(formattedExpression);
+
+            return func.Invoke(inputObject);
+
+        }
+
+
+    }
+
+}
---
+++ RuleSharp/TestConsoleApp/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RuleSharp;
+using RuleSharp.RuleEngine;
+using ThirdAssembly;
+using System.Reflection;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            var designation = new Designation()
+            {
+                Id = 1,
+                Name = "ManaGer",
+            };
+
+            var employee = new Employee
+            {
+                Id = 1,
+                Name = "Nejimon",
+                Designation = designation,
+                LstDesignation = new List<Designation>() { new Designation() { Id = 1, Name = "Fulano" } }
+            };
+
+            //employee.Teste.Find(r => r.Id == 1)
+            
+            //new Func<TestConsoleApp.Employee, bool>((input) => { return input.Numeros.Contains(10) });
+
+            var thirdAssemblyClass = new ClassFromAnotherAssembly
+            {
+                Id = 1
+            };
+
+            var runner = new RuleRunner();
+
+            var statementResult = runner.IsStatementTrue("input.Id == 1", employee);
+            var blockResult = runner.IsBlockTrue("if(input.Designation != null && input.Designation.Name != null && input.Designation.Name.ToLower() == \"manager\") return true; else return false;", employee);
+            var blockResult1 = runner.IsBlockTrue("return input.LstDesignation.Find(r => r.Name == \"Fulano\") != null;", employee);            
+
+
+            //demo of how types from another assembly can be used:
+            var runner2 = new RuleRunner(new List<Assembly>
+            {
+                typeof(ClassFromAnotherAssembly).Assembly //you need to add reference to the external assembly like this.
+            });
+
+            var statementResult2 = runner2.IsStatementTrue("input.Id == 1", thirdAssemblyClass);
+
+
+
+        }
+
+    }
+
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Designation Designation { get; set; }
+
+        public List<Designation> LstDesignation { get; set; }
+    }
+
+    public class Designation
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+
+
+
+}
