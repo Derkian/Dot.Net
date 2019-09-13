4a112d829db3b060145c7762d115f68ffbf91c39
derkian <rderkian@gmail.com>
2019-09-13 15:03:25 -0300

    Refactor

--- Regra/Regra/Program.cs
+++ Regra/Regra/Program.cs
@@ -96,7 +96,7 @@ namespace Regra
         {
             List<Regra> regras = new List<Regra>
             {
-                new Regra(1, "A", "Equal", "A", "System.String"),
+                new Regra(1, "True", "Equal", "True", "System.Boolean"),
                 new Regra(2, "A", "Equal", "B", "System.String"),
                 new Regra(3, 1, 2, "Or"),
                 new Regra(4, "C", "Equal", "D", "System.String"),
@@ -106,65 +106,71 @@ namespace Regra
                 new Regra(9, 8, 5, "And"), // COMECAR AQUI,
                 new Regra(10, "10,10", "Add", "20,20", "System.Double", true),
                 new Regra(11, 10, "Subtract", "2" , "System.Double", true), //COMECAR AQUI
-                new Regra(12, "10", "Equal", "20", "System.Int32")
+                new Regra(12, "10", "NotEqual", "20", "System.Int32"),
+                new Regra(13, 9, 12, "Or") // Todas as Regras
             };
+
+            var bla = aplicaRegra(13, regras);
 
             var result = aplicaRegra(5, regras);
             var resultA = aplicaRegra(8, regras);
             var resultB = aplicaRegra(9, regras);
-            var resultC = aplicaRegra(11, regras);
+            //var resultC = aplicaRegra(11, regras);
             var resultD = aplicaRegra(12, regras);
 
-            CustomRule.Executar();
+            //CustomRule.Executar();
         }
 
         static dynamic aplicaRegra(int IdRegra, List<Regra> lstRegras)
         {
-            Regra regra = lstRegras.FirstOrDefault(r => r.Id == IdRegra);
-            dynamic resultadoA = null;
-            dynamic resultadoB = null;
+            try
+            {
+                Regra regra = lstRegras.FirstOrDefault(r => r.Id == IdRegra);
+                dynamic resultadoA = null;
+                dynamic resultadoB = null;
 
-            if (regra == null)
-                return false;
+                if (regra == null)
+                    return false;
 
-            //REGRA VARIAVEL
-            if (regra.IdRegraVariavel > 0)
-                resultadoA = aplicaRegra(regra.IdRegraVariavel, lstRegras);
-            else
-                resultadoA = regra.ValorA;
+                //REGRA VARIAVEL
+                if (regra.IdRegraVariavel > 0)
+                    resultadoA = aplicaRegra(regra.IdRegraVariavel, lstRegras);
+                else
+                    resultadoA = regra.ValorA;
 
-            //REGRA CONDIÇÃO
-            if (regra.IdRegraCondicao > 0)
-                resultadoB = aplicaRegra(regra.IdRegraCondicao, lstRegras);
-            else
-                resultadoB = regra.ValorB;
+                //REGRA CONDIÇÃO
+                if (regra.IdRegraCondicao > 0)
+                    resultadoB = aplicaRegra(regra.IdRegraCondicao, lstRegras);
+                else
+                    resultadoB = regra.ValorB;
 
 
-            //VALIDAR OS TIPOS
-            if (
-                (!resultadoA.GetType().FullName.Equals(resultadoB.GetType().FullName)) ||
-                (
-                    !string.IsNullOrEmpty(regra.TipoDoValor) &&
+                //VALIDAR OS TIPOS
+                if (!string.IsNullOrEmpty(regra.TipoDoValor) && 
                     (
-                        !resultadoA.GetType().Equals(Type.GetType(regra.TipoDoValor)) || !resultadoB.GetType().Equals(Type.GetType(regra.TipoDoValor))
+                        (!resultadoA.GetType().FullName.Equals(resultadoB.GetType().FullName)) ||
+                        (!resultadoA.GetType().Equals(Type.GetType(regra.TipoDoValor)) || !resultadoB.GetType().Equals(Type.GetType(regra.TipoDoValor)))
                     )
                 )
-            )
-            {
-                resultadoA = converter(resultadoA, Type.GetType(regra.TipoDoValor));
-                resultadoB = converter(resultadoB, Type.GetType(regra.TipoDoValor));
+                {
+                    resultadoA = converter(resultadoA, Type.GetType(regra.TipoDoValor));
+                    resultadoB = converter(resultadoB, Type.GetType(regra.TipoDoValor));
+                }
+
+                //APLICA CALCULO
+                if (regra.RealizaCalculo)
+                {
+                    return compilarRegra(resultadoA, resultadoB, regra.Operacao);
+                }
+
+                return blnCompilarRegra(resultadoA, resultadoB, regra.Operacao);
             }
-
-            //APLICA CALCULO
-            if (regra.RealizaCalculo)
+            catch (Exception ex)
             {
-                return compilarRegra(resultadoA, resultadoB, regra.Operacao);
+                throw ex;
             }
-
-
-            return blnCompilarRegra(resultadoA, resultadoB, regra.Operacao);
         }
-        
+
         public static dynamic converter(dynamic objeto, Type tipoConvercao)
         {
             return Convert.ChangeType(objeto, tipoConvercao);
@@ -173,7 +179,9 @@ namespace Regra
         static bool blnCompilarRegra<T>(T objetoA, T objetoB, string operador)
         {
             var parametro = Expression.Parameter(typeof(T));
-            Expression expressao = criarExpressao<T>(objetoA, objetoB, operador, parametro);
+
+
+            Expression expressao = criarExpressao<T>(objetoB, operador, parametro);
 
             // build a lambda function User->bool and compile it
             return Expression.Lambda<Func<T, bool>>(expressao, parametro).Compile()(objetoA);
@@ -182,13 +190,13 @@ namespace Regra
         static T compilarRegra<T>(T objetoA, T objetoB, string operador)
         {
             var parametro = Expression.Parameter(typeof(T));
-            Expression expr = criarExpressao<T>(objetoA, objetoB, operador, parametro);
+            Expression expr = criarExpressao<T>(objetoB, operador, parametro);
 
             // build a lambda function User->bool and compile it
             return Expression.Lambda<Func<T, T>>(expr, parametro).Compile()(objetoA);
         }
 
-        static Expression criarExpressao<T>(T objetoA, T objetoB, string operador, ParameterExpression parametro)
+        static Expression criarExpressao<T>(T objetoB, string operador, ParameterExpression parametro)
         {
             Expression esquerda = parametro;
             Type typPropriedade = typeof(T);
@@ -197,7 +205,7 @@ namespace Regra
 
             if (ExpressionType.TryParse(operador, out operacao))
             {
-                var direita = Expression.Constant(Convert.ChangeType(objetoB, typPropriedade));
+                var direita = Expression.Constant(converter(objetoB, typPropriedade));
                 // use a binary operation, e.g. 'Equal' -> 'u.Age == 15'
                 return Expression.MakeBinary(operacao, esquerda, direita);
             }
@@ -205,7 +213,7 @@ namespace Regra
             {
                 var metodo = typPropriedade.GetMethod(operador);
                 var tyParametro = metodo.GetParameters()[0].ParameterType;
-                var direita = Expression.Constant(Convert.ChangeType(objetoB, tyParametro));
+                var direita = Expression.Constant(converter(objetoB, tyParametro));
                 // use a method call, e.g. 'Contains' -> 'u.Tags.Contains(some_tag)'
                 return Expression.Call(esquerda, metodo, direita);
             }
