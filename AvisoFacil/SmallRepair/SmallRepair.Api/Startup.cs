a2ae40f8e16c2d08dcfa4a218dc8763f38bcaa13
Rodrigo <Rodrigo@DESKTOP-53LDBI7>
2020-03-07 14:47:28 -0300

    EntityFramework

--- AvisoFacil/SmallRepair/SmallRepair.Api/Startup.cs
+++ AvisoFacil/SmallRepair/SmallRepair.Api/Startup.cs
@@ -5,10 +5,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using SmallRepair.Management.Context;
+using SmallRepair.Management.Repository;
 
 namespace SmallRepair.Api
 {
@@ -25,6 +28,13 @@ namespace SmallRepair.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SmallRepairDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString(""));
+            });
+
+            services.AddTransient<RepositoryEntity>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
